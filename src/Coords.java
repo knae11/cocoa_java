30b8c5caf4c2cf7208a0d5c5cd51b0e53d1aad3e
Naeun <knae11@naver.com>
2020-11-02 17:28:51 +0900

    feat : 조건문 연습 - 4.사분면

---
+++ src/Coords.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Coords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("x값을 입력하세요");
+        int x = sc.nextInt();
+        System.out.println("y값을 입력하세요");
+        int y = sc.nextInt();
+        sc.close();
+        if(x>0 && y>0){
+            System.out.println("1");
+
+        }
+        if(x<0 && y>0){
+            System.out.println("2");
+
+        }
+        if(x<0 && y<0){
+            System.out.println("3");
+
+        }
+        if(x>0 && y<0){
+            System.out.println("4");
+         
+        }
+    }
+}
