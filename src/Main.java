2c5457e85ec2d9628f628e581e6b152dfc395190
Naeun <knae11@naver.com>
2020-11-02 17:24:14 +0900

    feat : 조건문 연습 - 3.윤년

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("시작단를 입력하세요");
-        int start_number = sc.nextInt();
-        System.out.println("끝단을 입력하세요");
-        int end_number = sc.nextInt();
-
-        for(int j = start_number; j<= end_number; j++){
-            System.out.println(j +"단 만들기");
-            for(int i = 1; i <10; i++){
-                System.out.println(j +"*" + i + "=" + j*i);
-            }
-        }
+        System.out.println("main");
     }
 }
