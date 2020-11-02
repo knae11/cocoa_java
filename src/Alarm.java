dc31338ab1a26d47f411de3e65c25a1c9342a08d
Naeun <knae11@naver.com>
2020-11-02 17:57:19 +0900

    feat : 조건문 연습 - 5.시계

---
+++ src/Alarm.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Alarm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("시간을 입력하세요");
+        int hour = sc.nextInt();
+        int min = sc.nextInt();
+        min-=45;
+        if((min) < 0){
+            min +=60;
+            hour-=1;
+            if(hour <0){
+                hour = 23;
+            }
+        }
+
+
+
+        System.out.println(hour+ " " + min);
+    }
+}
