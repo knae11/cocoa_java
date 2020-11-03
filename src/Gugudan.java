ea03283977f970964f737e9673ac0a9a6ff0bcad
Naeun <knae11@naver.com>
2020-11-03 16:39:09 +0900

    refactor : 구구단 클래스 사용해서 나누기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,30 +2,29 @@ import java.util.Scanner;
 
 public class Gugudan {
 
-    static int[] getStartEndNumOfGugudan(){
+    public int start;
+    public int end;
+
+    public void getStartEndNumOfGugudan(){
         Scanner sc = new Scanner(System.in);
         System.out.println("시작단를 입력하세요");
-        int[] startAndEnd = new int[2];
-        startAndEnd[0] = sc.nextInt();
+        start = sc.nextInt();
         System.out.println("끝단을 입력하세요");
-        startAndEnd[1] = sc.nextInt();
+        end = sc.nextInt();
         sc.close();
-        return startAndEnd;
     }
-    static void printOneGugudan(int j){
+     private void printOneGugudan(int j){
         for (int i = 1; i < 10; i++) {
             System.out.println(j + "*" + i + "=" + j * i);
         }
     }
-    public static void main(String[] args) {
-
-        int[] startEnd =  getStartEndNumOfGugudan();
-
-        for (int j = startEnd[0]; j <= startEnd[1]; j++) {
+    public void printGugudans(){
+        for (int j = start; j <= end; j++) {
             System.out.println(j + "단 만들기");
             printOneGugudan(j);
             System.out.println();
         }
     }
+
 
 }
