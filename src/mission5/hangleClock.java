171d7cc66bbd7f9da84abfcaa8d1ad21beedc8f0
Naeun <knae11@naver.com>
2020-11-18 12:34:56 +0900

    mission 6 : 한글시계

--- src/mission5/hangleClock.java
+++ src/mission5/hangleClock.java
@@ -1,49 +1,148 @@
 package mission5;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class hangleClock {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
     private static final String[][] clock = {
-            {"한", "두", "세", "네", "다", "섯"},
-            {"여", "섯", "일", "곱", "여", "덟"},
-            {"아", "홉", "열", "한", "두", "시"},
+            {"한", "두", "세", "네", "다섯"},
+            {"여섯", "일곱", "여덟"},
+            {"아홉", "열", "한", "두", "시"},
             {"오", "이", "삼", "사", "오", "십"},
             {"전", "일", "이", "삼", "사", "오"},
             {"후", "육", "칠", "팔", "구", "분"}
     };
 
-    private static String[] now;
+    private static List<int[]> posInfo = new ArrayList<>();
 
+    private static void insertPos ( int i, int j){
+        int[] pos = { i, j};
+        posInfo.add(pos);
+    }
     public static void getTime() {
-        Date nowTime = new Date();
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
-        String time = timeFormat.format(nowTime);
-        now = time.split(":");
-        makeClock(now);
+        LocalDateTime currTime = LocalDateTime.now();
+        int hour = currTime.getHour();
+        int minute = currTime.getMinute();
+       // makeClock(3, 48);
+        makeClock(hour, minute);
+    }
+
+    private static void partHour(int hour) {
+        int numHour = hour % 12;
+        if (0 < numHour && numHour < 6) {
+            insertPos(0, numHour-1);
+            //System.out.println(clock[0][numHour - 1]);
+        }
+        switch (numHour) {
+            case 0:
+                insertPos(2,1);
+                insertPos(2,3);
+                //System.out.println(clock[2][1] + clock[2][3]);
+                break;
+            case 6:
+                insertPos(1,0);
+                //System.out.println(clock[1][0]);
+                break;
+            case 7:
+                insertPos(1,1);
+                //System.out.println(clock[1][1]);
+                break;
+            case 8:
+                insertPos(1,2);
+                //System.out.println(clock[1][2]);
+                break;
+            case 9:
+                insertPos(2,0);
+                //System.out.println(clock[2][0]);
+                break;
+            case 10:
+                insertPos(2,1);
+                //System.out.println(clock[2][1]);
+                break;
+            case 11:
+                insertPos(2,1);
+                insertPos(2,2);
+                //System.out.println(clock[2][1] + clock[2][2]);
+                break;
+        }
+        insertPos(2,4);
+        //System.out.println(clock[2][4]);
 
     }
 
-    private static void partHour(String hour) {
-        int numHour = Integer.parseInt(hour);
-//        if(numHour > 12)
+    private static void partDayOrNight(int hour) {
+        insertPos(3,0);
+        //System.out.println(clock[3][0]);
+        if (hour < 12) {
+            insertPos(4,0);
+          //  System.out.println(clock[4][0]);
+        } else {
+            insertPos(5,0);
+           // System.out.println(clock[5][0]);
+
+        }
+    }
+
+    private static void partMinute(int minute) {
+        int onesMinute = minute %10;
+        int tensMinute = minute / 10;
+        //십분단위
+        if( 1< tensMinute) {
+            insertPos(3,tensMinute-1);
+            //System.out.println(clock[3][tensMinute-1]);
+        }
+        if( 0< tensMinute){
+            insertPos(3,5);
+            //System.out.println(clock[3][5]);
+        }
+
+        //일분단위
+        if( 0<onesMinute && onesMinute <6  ){
+            insertPos(4,onesMinute);
+            //System.out.println(clock[4][onesMinute]);
+        }
+        if( 5< onesMinute && onesMinute < 10){
+            insertPos(5,onesMinute%5);
+            //System.out.println(clock[5][onesMinute%5]);
+        }
+        insertPos(5,5);
+        //System.out.println(clock[5][5]);
+
 
     }
 
-    private static void partMinute(String minute) {
-        System.out.println(minute);
+    private static void makeClock(int hour, int minute) {
+        partHour(hour);
+        partDayOrNight(hour);
+        partMinute(minute);
     }
-
-    private static void makeClock(String[] time) {
-        partHour(time[0]);
-        partMinute(time[1]);
+    private static void onColor (int i, int j){
+      System.out.print(ANSI_PURPLE + clock[i][j] + ANSI_RESET);
     }
 
     public static void print() {
-        for (int j = 0; j < clock.length; j++) {
-            for (int i = 0; i < clock[0].length; i++) {
-                System.out.print(clock[j][i]);
+        Collections.sort(posInfo, (e1, e2) ->{
+            if(e1[0] == e2[0]){
+                return e1[1] - e2[1];
+            }else{
+                return e1[0] -e2[0];
+            }
+        });
+        int count =0;
+        for (int i = 0; i < clock.length; i++) {
+            for (int j = 0; j < clock[i].length; j++) {
+                if( i == posInfo.get(count)[0] && j ==posInfo.get(count)[1]){
+                    onColor(i,j);
+                    count +=1;
+                    continue;
+                }
+                System.out.print(clock[i][j]);
             }
             System.out.println();
         }
