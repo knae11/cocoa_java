2a6c0e5cc1e5309db953d4b27bf995df89ef813c
Naeun <knae11@naver.com>
2020-11-19 14:04:32 +0900

    mission 6 : 정각일때 버그 해결, sort 람다 else문 제거

--- src/mission5/HClock.java
+++ src/mission5/HClock.java
@@ -29,6 +29,7 @@ public class HClock {
         LocalDateTime currTime = LocalDateTime.now();
         int hour = currTime.getHour();
         int minute = currTime.getMinute();
+
         makeClock(hour, minute);
     }
 
@@ -36,83 +37,69 @@ public class HClock {
         int numHour = hour % 12;
         if (0 < numHour && numHour < 6) {
             insertPos(0, numHour-1);
-            //System.out.println(clock[0][numHour - 1]);
         }
         switch (numHour) {
             case 0:
                 insertPos(2,1);
                 insertPos(2,3);
-                //System.out.println(clock[2][1] + clock[2][3]);
                 break;
             case 6:
                 insertPos(1,0);
-                //System.out.println(clock[1][0]);
                 break;
             case 7:
                 insertPos(1,1);
-                //System.out.println(clock[1][1]);
                 break;
             case 8:
                 insertPos(1,2);
-                //System.out.println(clock[1][2]);
                 break;
             case 9:
                 insertPos(2,0);
-                //System.out.println(clock[2][0]);
                 break;
             case 10:
                 insertPos(2,1);
-                //System.out.println(clock[2][1]);
                 break;
             case 11:
                 insertPos(2,1);
                 insertPos(2,2);
-                //System.out.println(clock[2][1] + clock[2][2]);
                 break;
         }
-        insertPos(2,4);
-        //System.out.println(clock[2][4]);
 
+        insertPos(2,4);
     }
 
     private void partDayOrNight(int hour) {
         insertPos(3,0);
-        //System.out.println(clock[3][0]);
         if (hour < 12) {
             insertPos(4,0);
-          //  System.out.println(clock[4][0]);
         } else {
             insertPos(5,0);
-           // System.out.println(clock[5][0]);
 
         }
     }
 
     private void partMinute(int minute) {
+        if(minute == 0 ){
+            return;
+        }
         int onesMinute = minute %10;
         int tensMinute = minute / 10;
         //십분단위
         if( 1< tensMinute) {
             insertPos(3,tensMinute-1);
-            //System.out.println(clock[3][tensMinute-1]);
         }
         if( 0< tensMinute){
             insertPos(3,5);
-            //System.out.println(clock[3][5]);
         }
 
         //일분단위
         if( 0<onesMinute && onesMinute <6  ){
             insertPos(4,onesMinute);
-            //System.out.println(clock[4][onesMinute]);
         }
         if( 5< onesMinute && onesMinute < 10){
             insertPos(5,onesMinute%5);
-            //System.out.println(clock[5][onesMinute%5]);
         }
-        insertPos(5,5);
-        //System.out.println(clock[5][5]);
 
+            insertPos(5,5);
 
     }
 
@@ -130,9 +117,8 @@ public class HClock {
         Collections.sort(posInfo, (e1, e2) ->{
             if(e1[0] == e2[0]){
                 return e1[1] - e2[1];
-            }else{
-                return e1[0] -e2[0];
             }
+            return e1[0] -e2[0];
         });
 
         int count =0;
@@ -141,7 +127,9 @@ public class HClock {
             for (int j = 0; j < clock[i].length; j++) {
                 if( i == posInfo.get(count)[0] && j ==posInfo.get(count)[1]){
                     onColor(i,j);
-                    count +=1;
+                    if(count < posInfo.size()-1){
+                        count +=1;
+                    }
                     continue;
                 }
                 System.out.print(clock[i][j]);
@@ -152,7 +140,6 @@ public class HClock {
 
 
     public void clockRun(){
-
         getTime();
         print();
     }
