de4e40a8c52d0f6d5b9b08d197081f6201c07abe
Naeun <knae11@naver.com>
2020-11-24 17:24:08 +0900

    mission 7 : 오목 frame 초기화

--- src/mission7and8/Main.java
+++ src/mission7and8/Main.java
@@ -2,10 +2,13 @@ package mission7and8;
 
 public class Main {
     public static void main(String[] args) {
-        /* Bingo Sample
+        /*
+        //Bingo Sample
         Bingo bingo = new Bingo();
         bingo.bingoSample();
          */
+        new Omok("Omok Game");
+
     }
 
 }
---
+++ src/mission7and8/Omok.java
@@ -0,0 +1,18 @@
+package mission7and8;
+
+import java.awt.*;
+
+public class Omok extends Frame {
+
+    public Omok(String title){
+        init(title);
+    }
+
+    private void init(String title) {
+        Frame f = new Frame(title);
+        f.setSize(500,500);
+        f.setVisible(true);
+    }
+
+
+}
