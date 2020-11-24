0ea8b42839efe29c8a52b0b8873b86fa5326e013
Naeun <knae11@naver.com>
2020-11-24 17:33:57 +0900

    mission 7 : window 닫기 이벤트 구현

--- src/mission7and8/Omok.java
+++ src/mission7and8/Omok.java
@@ -1,18 +1,31 @@
 package mission7and8;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Omok extends Frame {
-
+    private Frame f;
     public Omok(String title){
         init(title);
+        bindEvents();
     }
 
+
     private void init(String title) {
-        Frame f = new Frame(title);
+        f = new Frame(title);
         f.setSize(500,500);
         f.setVisible(true);
     }
 
+    private void bindEvents() {
+        f.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    });
+
+    }
 
 }
