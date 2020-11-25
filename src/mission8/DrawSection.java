cb818c7f2d5c2484c649f0259a57220d5413fa2b
Naeun <knae11@naver.com>
2020-11-25 17:20:00 +0900

    m8 : drawingSection 선자유롭게 그리기 기능

--- src/mission8/DrawSection.java
+++ src/mission8/DrawSection.java
@@ -1,10 +1,34 @@
 package mission8;
 
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class DrawSection extends Canvas {
-    public DrawSection(){
-        super();
-        setBackground(Color.white);
+
+    private int x;
+    private int y;
+
+    public DrawSection() {
+        setBackground(Color.orange);
+        bindEvents();
+
+    }
+
+    private void bindEvents() {
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                x = e.getX();
+                y = e.getY();
+                repaint();
+            }
+        });
+    }
+    public void update(Graphics g){
+        paint(g);
+    }
+    public void paint(Graphics g) {
+       g.drawString("*", x,y);
     }
 }
