a4a0bf2e3204f06417d992571c936e97fc5a80c0
Naeun <knae11@naver.com>
2020-11-27 12:06:20 +0900

    algo(leet): reverse

--- src/Algorithm/week4/AlgoWeek4.java
+++ src/Algorithm/week4/AlgoWeek4.java
@@ -1,7 +1,5 @@
 package Algorithm.week4;
 
-import java.util.Arrays;
-
 public class AlgoWeek4 {
     //https://leetcode.com/problems/two-sum/
     public static int[] twoSum(int[] nums, int target) {
@@ -18,9 +16,25 @@ public class AlgoWeek4 {
         }
         return answer;
     }
+
+    //https://leetcode.com/problems/reverse-integer/
+    //인터넷에서 알게된 코드인데 뒷자리부터 하나씩 빼서 처리하고 있다..!! 똑똑하다..! 문자열로 안 바꿔도 되서 좋다!
+    //answer을 int로 하면 범위 넘어가서 long으로 해주어야 함, 문제 상세조건 예외처리 잘 읽기!!
+    public static int reverse(int x) {
+        long answer = 0;
+        while(x!=0){
+            int digit = x % 10;
+            answer = answer*10 + digit;
+            if( answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE ){
+                return 0;
+            }
+            x /= 10;
+        }
+        return (int)answer;
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println((reverse( 1534236469)));
     }
 }
