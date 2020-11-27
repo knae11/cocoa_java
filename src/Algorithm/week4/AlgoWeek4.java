4f7dd9e6f6992f1cc68730e15864ea6af317cb3b
Naeun <knae11@naver.com>
2020-11-27 14:50:24 +0900

    algo(leet): longestCommonPrefix

--- src/Algorithm/week4/AlgoWeek4.java
+++ src/Algorithm/week4/AlgoWeek4.java
@@ -9,9 +9,9 @@ public class AlgoWeek4 {
     public static int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
 
-        for ( int i = 0; i < nums.length; i++ ) {
-            for ( int j = i+1; j < nums.length; j++ ){
-                if( nums[i] + nums[j] == target ){
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
                     answer[0] = i;
                     answer[1] = j;
                     return answer;
@@ -26,15 +26,15 @@ public class AlgoWeek4 {
     //answer을 int로 하면 범위 넘어가서 long으로 해주어야 함, 문제 상세조건 예외처리 잘 읽기!!
     public static int reverse(int x) {
         long answer = 0;
-        while(x!=0){
+        while (x != 0) {
             int digit = x % 10;
-            answer = answer*10 + digit;
-            if( answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE ){
+            answer = answer * 10 + digit;
+            if (answer > Integer.MAX_VALUE || answer < Integer.MIN_VALUE) {
                 return 0;
             }
             x /= 10;
         }
-        return (int)answer;
+        return (int) answer;
     }
 
     //https://leetcode.com/problems/palindrome-number/
@@ -45,10 +45,10 @@ public class AlgoWeek4 {
         String reversedX = "";
         String stringX = String.valueOf(x);
         String[] stringArr = stringX.split("");
-        for ( int i = stringArr.length-1; i > -1; i-- ){
+        for (int i = stringArr.length - 1; i > -1; i--) {
             reversedX += stringArr[i];
         }
-        if ( stringX.equals(reversedX) ){
+        if (stringX.equals(reversedX)) {
             answer = true;
         }
         return answer;
@@ -68,8 +68,8 @@ public class AlgoWeek4 {
         romanNum.put("D", 500);
         romanNum.put("M", 1000);
         String[] sArr = s.split("");
-        for ( int i = sArr.length-1; i > -1 ; i--){
-            if( i != sArr.length-1 && romanNum.get(sArr[i]) < romanNum.get(sArr[i+1])) {
+        for (int i = sArr.length - 1; i > -1; i--) {
+            if (i != sArr.length - 1 && romanNum.get(sArr[i]) < romanNum.get(sArr[i + 1])) {
                 answer -= romanNum.get(sArr[i]);
                 continue;
             }
@@ -78,7 +78,49 @@ public class AlgoWeek4 {
         return answer;
     }
 
+    //https://leetcode.com/problems/longest-common-prefix/
+    //내 풀이ㅠㅠ -> solution과 차이
+    /*
+    * leetcode java solution
+    public static String solongestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++){
+            //안 일치하면 -1이 뜸! strs[i].indexOf(prefix) == 0 인 경우는 prefix가 그 문자열에 있을 때!
+            while (strs[i].indexOf(prefix) != 0) {
+                //prefix에서 하나씩 빼가면서 prefix가 존재할 때까지 계산!
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }}
+        return prefix;
+    }
+    * */
+    public static String longestCommonPrefix(String[] strs) {
+        if( strs.length == 0){
+            return "";
+        }
+        String answer = strs[0];
+        String[] answerArr = answer.split("");
+        for (int i = 1; i < strs.length; i++) {
+            String prefix = "";
+            for (int j = 0; j < Math.min(strs[i].length(), answer.length()); j++) {
+                if (answerArr[j].equals(strs[i].split("")[j])) {
+                    prefix += answerArr[j];
+                    continue;
+                }
+                break;
+            }
+            answer = prefix;
+            if(answer.equals("")){
+                return answer;
+            }
+        }
+        return answer;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(romanToInt( "MCMXCIV"));
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
     }
 }
