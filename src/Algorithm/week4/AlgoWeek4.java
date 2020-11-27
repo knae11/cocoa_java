68590cfc988b076f3a64315f356408d539be7a2d
Naeun <knae11@naver.com>
2020-11-27 11:46:51 +0900

    algo(leet): twoSum

---
+++ src/Algorithm/week4/AlgoWeek4.java
@@ -0,0 +1,26 @@
+package Algorithm.week4;
+
+import java.util.Arrays;
+
+public class AlgoWeek4 {
+    //https://leetcode.com/problems/two-sum/
+    public static int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+
+        for ( int i = 0; i < nums.length; i++ ) {
+            for ( int j = i+1; j < nums.length; j++ ){
+                if( nums[i] + nums[j] == target ){
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+}
