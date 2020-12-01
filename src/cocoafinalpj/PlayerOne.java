6a40dde400ffbb0867ad3c07e55182c4e9a6044f
Naeun <knae11@naver.com>
2020-12-01 17:36:48 +0900

    feat: 여우 상태에 따라 다른 이미지 보여주기

--- src/cocoafinalpj/PlayerOne.java
+++ src/cocoafinalpj/PlayerOne.java
@@ -7,35 +7,58 @@ public class PlayerOne extends Character implements Runnable {
 
     private int playerX = Main.INIT_X;
     private int playerY = Main.INIT_Y;
-
-
+    private FoxStatus foxStatus;
     private Image playerOne;
+    private Image playerOneJump;
+    private Image playerOneDown;
+
     private String pathname = "src/cocoafinalpj/image/fox/leftwalk1.png";
+    private String jumpImage = "src/cocoafinalpj/image/fox/jump_left.png";
+    private String downImage ="src/cocoafinalpj/image/fox/down_left.png";
 
     public PlayerOne() {
         super();
         playerOne = makeCharacter(pathname);
+        playerOneJump = makeCharacter(jumpImage);
+        playerOneDown = makeCharacter(downImage);
+        foxStatus = FoxStatus.WALK_RIGHT;
     }
 
 
     public void keyProcess() {
         if (up && playerY - playerSpeed > 0) {
             playerY -= playerSpeed;
+            foxStatus = FoxStatus.UP;
         }
         if (down && playerY + playerSpeed < Main.INIT_Y) {
             playerY += playerSpeed;
+            foxStatus = FoxStatus.DOWN;
         }
         if (left && playerX - playerSpeed > 0) {
             playerX -= playerSpeed;
+            foxStatus = FoxStatus.WALK_LEFT;
         }
         if (right && playerX + playerSpeed + Main.CHAR_WIDTH < Main.BOARD_WIDTH / 2) {
             playerX += playerSpeed;
+            foxStatus = FoxStatus.WALK_RIGHT;
         }
     }
 
 
-    public void playerDraw(Graphics g) {
-        g.drawImage(playerOne, playerX, playerY, null);
+    public void playerDraw(Graphics g) {switch (foxStatus) {
+        case UP:
+            g.drawImage(playerOneJump, playerX, playerY, null);
+            break;
+        case WALK_RIGHT:
+        case WALK_LEFT:
+            g.drawImage(playerOne, playerX, playerY, null);
+            break;
+        case DOWN:
+            g.drawImage(playerOneDown, playerX, playerY, null);
+            break;
+        default:
+            break;
+    }
     }
 
     @Override
--- src/cocoafinalpj/PlayerTwo.java
+++ src/cocoafinalpj/PlayerTwo.java
@@ -6,40 +6,66 @@ import java.awt.Image;
 public class PlayerTwo extends Character implements Runnable {
 
 
-    private int playerX = Main.BOARD_WIDTH-(Main.INIT_X+Main.CHAR_WIDTH);
+    private int playerX = Main.BOARD_WIDTH - (Main.INIT_X + Main.CHAR_WIDTH);
     private int playerY = Main.INIT_Y;
 
+    private FoxStatus foxStatus;
     private Image playerTwo;
-    private String pathname = "src/cocoafinalpj/image/fox/rightwalk1.png";
-    public PlayerTwo(){
+    private Image playerTwoJump;
+    private Image playerTwoDown;
+    private String defaultImage = "src/cocoafinalpj/image/fox/rightwalk1.png";
+    private String jumpImage = "src/cocoafinalpj/image/fox/jump_right.png";
+    private String downImage ="src/cocoafinalpj/image/fox/down_right.png";
+
+    public PlayerTwo() {
         super();
-        playerTwo = makeCharacter(pathname);
+        playerTwo = makeCharacter(defaultImage);
+        playerTwoJump = makeCharacter(jumpImage);
+        playerTwoDown = makeCharacter(downImage);
+        foxStatus = FoxStatus.WALK_LEFT;
 
     }
 
     public void keyProcess() {
-        if (up && playerY - playerSpeed> 0) {
+        if (up && playerY - playerSpeed > 0) {
             playerY -= playerSpeed;
+            foxStatus = FoxStatus.UP;
         }
-        if(down && playerY +playerSpeed < Main.INIT_Y){
+        if (down && playerY + playerSpeed < Main.INIT_Y) {
             playerY += playerSpeed;
+            foxStatus = FoxStatus.DOWN;
         }
-        if ( left && playerX - playerSpeed >Main.BOARD_WIDTH/2){
+        if (left && playerX - playerSpeed > Main.BOARD_WIDTH / 2) {
             playerX -= playerSpeed;
+            foxStatus = FoxStatus.WALK_LEFT;
         }
-        if( right && playerX + playerSpeed +Main.CHAR_WIDTH  < Main.BOARD_WIDTH ){
+        if (right && playerX + playerSpeed + Main.CHAR_WIDTH < Main.BOARD_WIDTH) {
             playerX += playerSpeed;
+            foxStatus = FoxStatus.WALK_RIGHT;
         }
     }
 
 
-    public void playerDraw(Graphics g){
-        g.drawImage(playerTwo, playerX, playerY, null);
+    public void playerDraw(Graphics g) {
+        switch (foxStatus) {
+            case UP:
+                g.drawImage(playerTwoJump, playerX, playerY, null);
+                break;
+            case WALK_RIGHT:
+            case WALK_LEFT:
+                g.drawImage(playerTwo, playerX, playerY, null);
+                break;
+            case DOWN:
+                g.drawImage(playerTwoDown, playerX, playerY, null);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void run() {
-        while(true){
+        while (true) {
             try {
                 keyProcess();
                 //System.out.println("thread 2 going");
