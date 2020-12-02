7da83fcac9b3b40edb1c32815adb43a671f271a8
Naeun <knae11@naver.com>
2020-12-02 11:08:43 +0900

    feat: 공 프레임 안에서 움직이기 구현

--- src/cocoafinalpj/Ball.java
+++ src/cocoafinalpj/Ball.java
@@ -5,25 +5,48 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 
 public class Ball {
+
     private Image ball;
     private int ballX = 100;
-    private int ballY =100;
+    private int ballY = 100;
     private int ballSpeed = 5;
-    private int ballSpeedUp = ballSpeed*2;
+    private int ballSpeedUp = ballSpeed * 2;
+    boolean goLeftX = true;
+    boolean goDownY = true;
 
-
-    public Ball(){
+    public Ball() {
         ball = new ImageIcon("src/cocoafinalpj/image/ball.png").getImage();
     }
 
-    public void ballDraw(Graphics g){
+    public void ballDraw(Graphics g) {
         g.drawImage(ball, ballX, ballY, null);
         moveBall();
     }
+
     //TODO: 공 움직이기 구현하기!
     private void moveBall() {
-        ballX += ballSpeed;
-        ballY+= ballSpeed;
+        if (goDownY) {
+            ballY += ballSpeed;
+        } else {
+            ballY -= ballSpeed;
+        }
+        if (goLeftX) {
+            ballX += ballSpeed;
+        } else {
+            ballX -= ballSpeed;
+        }
+        if (ballX + Main.BALL_SIZE == Main.BOARD_WIDTH) {
+            goLeftX = false;
+        }
+        if (ballX == 0) {
+            goLeftX = true;
+        }
+        if (ballY + Main.BALL_SIZE == Main.BOARD_HEIGHT) {
+            goDownY = false;
+        }
+        if (ballY == 0) {
+            goDownY = true;
+        }
     }
 
 }
